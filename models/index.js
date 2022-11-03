ef02afecdc2c223addf0987ba124daf3ac68c623
Zaymus <carson.brown47@gmail.com>
2022-11-03 12:49:58 -0400

    added module export moddeling to impoort all or groups of models in future files

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const inventory = require("./inventory");
+const invoice = require("./invoice");
+const location = require("./location");
+const payments = require("./payments");
+const personnel = require("./personnel");
+
+module.exports = [inventory, invoice, location, payments, personnel];
---
+++ models/inventory/index.js
@@ -0,0 +1,5 @@
+const batch = require("./batch");
+const inventory = require("./inventory");
+const product = require("./product");
+
+module.exports = [batch, inventory, product];
