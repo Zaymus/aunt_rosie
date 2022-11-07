42093d53d81627c63ece0d7e22d7fb6cb995ec12
Zaymus <carson.brown47@gmail.com>
2022-11-07 17:52:19 -0500

    fixed env import

--- util/database.js
+++ util/database.js
@@ -1,11 +1,9 @@
 const Sequelize = require("sequelize");
-const env = require("./constants");
-
-const env = process.env;
+const { env } = require("./constants");
 
 const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
-	dialect: "mysql",
 	host: env.DB_HOST,
+	dialect: "mysql",
 });
 
 module.exports = sequelize;
