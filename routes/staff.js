d165ef01d70d37c811f88a54e3d7efbdec34f6b6
Zaymus <carson.brown47@gmail.com>
2022-11-08 16:40:46 -0500

    added CRUD operations for staff and hourly_rate along with postman test coverage

--- routes/staff.js
+++ routes/staff.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const staffController = require("../controllers/staff");
+
+router.get("/all", staffController.getStaff);
+
+router.post("/new", staffController.postNewStaff);
+
+router.get("/:staffId", staffController.getStaffbyId);
+
+router.put("/:staffId", staffController.putDeleteStaff);
+
+router.patch("/:staffId", staffController.patchEditStaff);
 
 module.exports = router;
