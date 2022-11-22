fb2b0f44e11e636001652415d8b515c5bd926837
Zaymus <carson.brown47@gmail.com>
2022-11-22 15:27:37 -0500

    completed edit and create product routes, just need to add batch creation call to complete create route and product flow

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -10,26 +10,46 @@ const {
 } = require("../models/location");
 const { getCurrentDate, addDays } = require("../util/date");
 const sequelize = require("../util/database");
+const { env } = require("../util/constants");
+const axios = require("axios");
+
+exports.getNewProduct = (req, res, next) => {
+	shelf.findAll()
+		.then(shelves => {
+			res.render('product/new-product', {
+				pageTitle: "Create Product",
+				selected: "inventory",
+				locations: shelves
+			})
+		})
+		.catch(err => {
+			res.status(400).json({err});
+		})
+}
 
 exports.postNewProduct = (req, res, next) => {
-	const name = req.body.name;
+	const productName = req.body.product_name;
 	const shelfLife = req.body.shelf_life;
-	const ingredients = req.body.ingredients;
+	const ingredients = req.body.ingredients.split(",").join("|");
 	const salePrice = req.body.sale_price;
 	const productCost = req.body.product_cost;
-	const storageType = req.body.storage_type;
 
 	product
 		.create({
-			name: name,
+			product_name: productName,
 			shelf_life: shelfLife,
 			ingredients: ingredients,
 			sale_price: salePrice,
-			product_cost: productCost,
-			storage_type: storageType,
+			product_cost: productCost
 		})
-		.then((result) => {
-			res.json({ result });
+		.then((prod) => {
+			product.sync()
+				.then(result => {
+					res.redirect(`/inventory/${prod.dataValues.product_id}`);
+				})
+				.catch(err => {
+					res.status(400).json({err});
+				})
 		})
 		.catch((err) => {
 			res.status(400).json({ err });
@@ -76,6 +96,83 @@ exports.getProductById = (req, res, next) => {
 	// 		res.status(400).json({ err });
 	// 	});
 };
+
+exports.getUpdateProduct = (req, res, next) => {
+	const id = req.params.productId;
+
+	sequelize
+		.query('CALL getProductDataFromId(:param_id)',
+    	{replacements: {param_id: id}})
+		.then(result => {
+			console.log(result);
+			const productData = result;
+			shelf.findAll()
+				.then(shelves => {
+					console.log(productData, shelves);
+					res.render('product/edit-product', {
+						pageTitle: result[0].product_name,
+						selected: 'inventory',
+						product: result[0],
+						locations: shelves
+					});
+				})
+				.catch(err => {
+					console.log("test");
+					res.status(400).json({err});
+				});
+		})
+		.catch(err => {
+			res.status(400).json({err});
+		})
+}
+
+exports.postUpdateProduct = (req, res, next) => {
+	const id = req.params.productId;
+	product.findByPk(id)
+		.then((product) => {
+			let {shelf, ...productData} = req.body;
+			productData.ingredients = productData.ingredients.split(",").join("|");
+			productData.product_name = product.product_name;
+			
+			axios.patch(`${env.BASE_URL}/inventory/${id}`, {
+				product_name: productData.product_name,
+				...productData
+			})
+			.then((result) => {
+				inventory.findAll({where: {product_id: id}})
+					.then(result => {
+						const batchId = result[0].dataValues.batch_id;
+						batch.findByPk(batchId)
+							.then(batch => {
+								batch.set({...batch, batch_location: shelf});
+								batch.save()
+									.then(result => {
+										res.redirect(`/inventory/${id}`);
+									})
+									.catch(err => {
+										res.status(400).json({err});
+									});
+							})
+							.catch(err => {
+								console.log(4);
+								res.status(400).json({err});
+							});
+					})
+					.catch(err => {
+						console.log(3);
+						res.status(400).json({err});
+					});
+			})
+			.catch((err) => {
+				console.log(2);
+				res.status(400).json({err});
+			});
+		})
+		.catch((err) => {
+			console.log(1);
+			res.status(400).json({err})
+	});
+}
 
 exports.patchUpdateProduct = (req, res, next) => {
 	const id = req.params.productId;
