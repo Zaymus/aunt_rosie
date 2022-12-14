1bc08715dc85f780bb732f15632e4edeb40ab84d
Zaymus <carson.brown47@gmail.com>
2022-12-14 17:04:29 -0500

    fixed discount bug on invoice screen

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,18 +1,18 @@
-const { invoice } = require("../models/invoice");
-const {getCurrentDate} = require("../util/date");
-const product = require("../models/inventory/product");
-const crypto = require("crypto");
-const nodemailer = require("nodemailer");
-const {env} = require("../util/constants");
-const sequelize = require("../util/database");
+const { invoice } = require('../models/invoice');
+const { getCurrentDate } = require('../util/date');
+const product = require('../models/inventory/product');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const { env } = require('../util/constants');
+const sequelize = require('../util/database');
 
 var transporter = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: env.MAILTRAP_USER,
-    pass: env.MAILTRAP_PASS
-  }
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	auth: {
+		user: env.MAILTRAP_USER,
+		pass: env.MAILTRAP_PASS,
+	},
 });
 
 const algorithm = 'aes-256-ctr';
@@ -20,120 +20,155 @@ const secretKey = env.ENC_SECRET;
 
 const encrypt = (data) => {
 	const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
-  const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
+	const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+	const encrypted = Buffer.concat([cipher.update(data), cipher.final()]);
 
-  return {
-    iv: iv.toString('hex'),
-    content: encrypted.toString('hex')
+	return {
+		iv: iv.toString('hex'),
+		content: encrypted.toString('hex'),
 	};
-}
+};
 
-const decrypt = hash => {
-  const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
+const decrypt = (hash) => {
+	const decipher = crypto.createDecipheriv(
+		algorithm,
+		secretKey,
+		Buffer.from(hash.iv, 'hex')
+	);
 
-  const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
+	const decrpyted = Buffer.concat([
+		decipher.update(Buffer.from(hash.content, 'hex')),
+		decipher.final(),
+	]);
 
-  return decrpyted.toString();
-}
-
+	return decrpyted.toString();
+};
 
 exports.getIndex = (req, res, next) => {
-	sequelize.query("CALL lowQuantityPreview(6);")
-		.then(result => {
+	sequelize
+		.query('CALL lowQuantityPreview(6);')
+		.then((result) => {
 			return result;
 		})
-		.then(lowQty => {
-			sequelize.query("CALL soonToExpirePreview(500);")
-			.then(result => {
-				return {
-					quantity: lowQty,
-					expire: result,
-				};
-			})
-			.then(data => {
-				res.render("index", {
-					pageTitle: "Dashboard",
-					selected: "none",
-					lowQuantity: data.quantity,
-					expireSoon: data.expire,
+		.then((lowQty) => {
+			sequelize
+				.query('CALL soonToExpirePreview(500);')
+				.then((result) => {
+					return {
+						quantity: lowQty,
+						expire: result,
+					};
+				})
+				.then((data) => {
+					res.render('index', {
+						pageTitle: 'Dashboard',
+						selected: 'none',
+						lowQuantity: data.quantity,
+						expireSoon: data.expire,
+					});
+				})
+				.catch((err) => {
+					res.status(400).json({ err });
 				});
-			})
-			.catch(err => {
-				res.status(400).json({err});
-			})
 		})
-		.catch(err => {
-			res.status(400).json({err});
+		.catch((err) => {
+			res.status(400).json({ err });
 		});
 };
 
 exports.getShop = (req, res, next) => {
-	product.findAll()
-		.then(products => {
-			res.render("shop/shop", {
-				pageTitle: "Shop",
-				selected: "sale",
-				products: products
+	product
+		.findAll()
+		.then((products) => {
+			res.render('shop/shop', {
+				pageTitle: 'Shop',
+				selected: 'sale',
+				products: products,
 			});
 		})
-		.catch(err => {
-			res.json({err});
+		.catch((err) => {
+			res.json({ err });
 		});
 };
 
 exports.postShop = (req, res, next) => {
 	const saleData = JSON.parse(req.body.cart);
-	invoice.create({
-		date: getCurrentDate(), 
-		amount: saleData.totals.grandtotal,
-		discount: saleData.totals.discount,
-	})
-	.then(result => {
-		let data = JSON.stringify({
-			invoice: result.dataValues, 
-			cart: saleData.cart,
-			totals: saleData.totals
+	console.log(saleData);
+	invoice
+		.create({
+			date: getCurrentDate(),
+			amount: saleData.totals.grandtotal,
+			discount: saleData.totals.discount,
+		})
+		.then((result) => {
+			let data = JSON.stringify({
+				invoice: result.dataValues,
+				cart: saleData.cart,
+				totals: saleData.totals,
+			});
+
+			const encryptedData = encrypt(Buffer.from(data, 'utf8'));
+
+			res.redirect(
+				`/shop/invoice?iv=${encryptedData.iv}&content=${encryptedData.content}`
+			);
+		})
+		.catch((err) => {
+			res.status(400).json({ err });
 		});
-
-		const encryptedData = encrypt(Buffer.from(data, 'utf8'));
-
-		res.redirect(`/shop/invoice?iv=${encryptedData.iv}&content=${encryptedData.content}`);
-	})
-	.catch(err => {
-		res.status(400).json({err});
-	})
-}
+};
 
 exports.getInvoice = (req, res, next) => {
-	let data = {iv: req.query.iv, content:req.query.content}
+	let data = { iv: req.query.iv, content: req.query.content };
 	let invoiceData = JSON.parse(decrypt(data));
 	res.render('shop/invoice', {
-		pageTitle: "invoice",
+		pageTitle: 'invoice',
 		selected: 'none',
 		invoice: invoiceData.invoice,
 		cart: invoiceData.cart,
 		totals: invoiceData.totals,
 	});
-}
+};
 
 exports.postInvoice = (req, res, next) => {
-	let data = {iv: req.query.iv, content:req.query.content}
+	let data = { iv: req.query.iv, content: req.query.content };
 	let invoiceData = JSON.parse(decrypt(data));
 
 	let cart = invoiceData.cart;
 	let cartData = '';
 
-	cart.forEach(item => {
-		let name = item.name.replace(/&amp;/g , "&");
-		cartData += `<span>${name}</span><span>$${parseFloat(item.price).toFixed(2)}</span><span>${item.quantity}</span><span>$${item.total.toFixed(2)}</span>`;
+	cart.forEach((item) => {
+		let name = item.name.replace(/&amp;/g, '&');
+		cartData += `<span>${name}</span><span>$${parseFloat(item.price).toFixed(
+			2
+		)}</span><span>${item.quantity}</span><span>$${item.total.toFixed(
+			2
+		)}</span>`;
 	});
 
 	transporter.sendMail({
 		to: req.body.email,
 		from: 'no-reply@auntrosies.com',
 		subject: `Invoice #${invoiceData.invoice.invoice_id}`,
-		html: `<style>.invoice--container {width: 100%;height: 80vh;position: fixed;top: 20vh;left: 0;display: flex;flex-direction: column;align-items: center;margin: 0px auto;overflow-y: scroll;}.invoice--header {width: 60%;height: 30%;display: flex;flex-direction: row;justify-content: space-between;align-items: center;}.invoice--header .logo {height: 100%;}.invoice--header img {height: 100%;}.invoice--body {width: 60%;display: grid;grid-template-columns: 2fr 1fr 1fr 1fr;grid-auto-rows: 50px;margin: 30px 0px;text-align: center;}.invoice--body .heading {font-size: 24pt;}.invoice--totals .totals {background-color: var(--clr-bg);}</style><div class="invoice--container"><div class="invoice--header"><div class="invoice--info"><h2>Invoice #${invoiceData.invoice.invoice_id}</h2><h3>Issued: ${new Date(invoiceData.invoice.date).toDateString()}</h3><h3>Payment Type: Cash</h3></div><div class="logo"><img src="http://localhost:3000/images/logo_svg.svg"></div></div><div class="invoice--body"><span class="bold heading">Product</span><span class="bold heading">Price</span><span class="bold heading">Quantity</span><span class="bold heading">Total</span>${cartData}</div><div class="invoice--totals"><div class="totals"><div class="sub-total"><span class="bold">Sub total: </span><span class="sub-total-amount">${parseFloat(invoiceData.totals.subtotal).toFixed(2)}</span></div><div class="discount"><span class="bold">Discount: </span><span class="discount-amount">${ parseFloat(invoiceData.totals.discount).toFixed(2)}</span></div><div class="tax"><span class="bold">Tax: </span><span class="tax-amount">${parseFloat(invoiceData.totals.tax).toFixed(2)}</span></div><div class="grand-total"><span class="bold">Grand total: </span><span class="grand-total-amount">${parseFloat(invoiceData.totals.grandtotal).toFixed(2)}</span></div></div></div></div>`
-	})
-	res.redirect("/");
-}
+		html: `<style>.invoice--container {width: 100%;height: 80vh;position: fixed;top: 20vh;left: 0;display: flex;flex-direction: column;align-items: center;margin: 0px auto;overflow-y: scroll;}.invoice--header {width: 60%;height: 30%;display: flex;flex-direction: row;justify-content: space-between;align-items: center;}.invoice--header .logo {height: 100%;}.invoice--header img {height: 100%;}.invoice--body {width: 60%;display: grid;grid-template-columns: 2fr 1fr 1fr 1fr;grid-auto-rows: 50px;margin: 30px 0px;text-align: center;}.invoice--body .heading {font-size: 24pt;}.invoice--totals .totals {background-color: var(--clr-bg);}</style><div class="invoice--container"><div class="invoice--header"><div class="invoice--info"><h2>Invoice #${
+			invoiceData.invoice.invoice_id
+		}</h2><h3>Issued: ${new Date(
+			invoiceData.invoice.date
+		).toDateString()}</h3><h3>Payment Type: Cash</h3></div><div class="logo"><img src="http://localhost:3000/images/logo_svg.svg"></div></div><div class="invoice--body"><span class="bold heading">Product</span><span class="bold heading">Price</span><span class="bold heading">Quantity</span><span class="bold heading">Total</span>${cartData}</div><div class="invoice--totals"><div class="totals"><div class="sub-total"><span class="bold">Sub total: </span><span class="sub-total-amount">${parseFloat(
+			invoiceData.totals.subtotal
+		).toFixed(
+			2
+		)}</span></div><div class="discount"><span class="bold">Discount: </span><span class="discount-amount">${parseFloat(
+			invoiceData.totals.discount
+		).toFixed(
+			2
+		)}</span></div><div class="tax"><span class="bold">Tax: </span><span class="tax-amount">${parseFloat(
+			invoiceData.totals.tax
+		).toFixed(
+			2
+		)}</span></div><div class="grand-total"><span class="bold">Grand total: </span><span class="grand-total-amount">${parseFloat(
+			invoiceData.totals.grandtotal
+		).toFixed(2)}</span></div></div></div></div>`,
+	});
+	res.redirect('/');
+};
--- public/js/main.js
+++ public/js/main.js
@@ -1,183 +1,197 @@
-const products = document.querySelectorAll(".product");
-const productList = document.querySelector(".item-list");
+const products = document.querySelectorAll('.product');
+const productList = document.querySelector('.item-list');
 
-var discountAmount = 0.00;
-var prev = 0.00;
+var discountAmount = 0.0;
+var prev = 0.0;
 const totals = {
-  subtotal: 0,
-  discount: 0,
-  tax: 0,
-  grandtotal: 0,
+	subtotal: 0,
+	discount: 0,
+	tax: 0,
+	grandtotal: 0,
 };
 
 const removeItem_onClick = (remove) => {
-  const id = parseInt(remove.target.getAttribute("id"));
-  const prod = document.querySelector(`.product[data-id='${id}']`)
-  const qty = document.querySelector(`#qty-${id}`);
-  
-  prod.setAttribute("data-quantity", parseInt(prod.dataset.quantity) - 1);
+	const id = parseInt(remove.target.getAttribute('id'));
+	const prod = document.querySelector(`.product[data-id='${id}']`);
+	const qty = document.querySelector(`#qty-${id}`);
 
-  if(prod.dataset.quantity != 0)
-  {
-    document.getElementById(id).setAttribute("data-quantity", prod.dataset.quantity);
-    qty.innerHTML = `Qty: ${prod.dataset.quantity}`;
-  } else {
-    document.getElementById(id).remove();
-  }
+	prod.setAttribute('data-quantity', parseInt(prod.dataset.quantity) - 1);
 
-  updateTotals();
-}
+	if (prod.dataset.quantity != 0) {
+		document
+			.getElementById(id)
+			.setAttribute('data-quantity', prod.dataset.quantity);
+		qty.innerHTML = `Qty: ${prod.dataset.quantity}`;
+	} else {
+		document.getElementById(id).remove();
+	}
+
+	updateTotals();
+};
 
 const updateCart = () => {
-  const items = document.querySelectorAll('.item');
-  const cartField = document.querySelector('input[name="cart"]');
-  const cart = [];
-  items.forEach((item) => {
-    const prodData = document.querySelector(`[data-id="${item.getAttribute("id")}"]`);
-    let id = item.id;
-    let name = item.childNodes[1].innerHTML;
-    let quantity = item.childNodes[2].innerHTML.split(" ")[1];
-    let price = prodData.dataset.saleprice;
-    let total = price * quantity;
-    let cartItem = {
-      id: id,
-      name: name,
-      quantity: quantity,
-      price: price,
-      total: total,
-    };
-    cart.push(cartItem);
-  });
-  cartField.setAttribute("value", JSON.stringify({cart: cart, totals: totals}));
-}
+	const items = document.querySelectorAll('.item');
+	const cartField = document.querySelector('input[name="cart"]');
+	const cart = [];
+	items.forEach((item) => {
+		const prodData = document.querySelector(
+			`[data-id="${item.getAttribute('id')}"]`
+		);
+		let id = item.id;
+		let name = item.childNodes[1].innerHTML;
+		let quantity = item.childNodes[2].innerHTML.split(' ')[1];
+		let price = prodData.dataset.saleprice;
+		let total = price * quantity;
+		let cartItem = {
+			id: id,
+			name: name,
+			quantity: quantity,
+			price: price,
+			total: total,
+		};
+		cart.push(cartItem);
+	});
+	cartField.setAttribute(
+		'value',
+		JSON.stringify({ cart: cart, totals: totals })
+	);
+};
 
 const product_onClick = (product) => {
-  const items = Array.prototype.slice.call(productList.childNodes);
-  const prod = product.path[product.path.length - 7];
-  const dupe = items.filter(item => item.getAttribute("id") == prod.dataset.id);
-  prod.dataset.quantity = parseInt(prod.dataset.quantity) + 1;
+	const items = Array.prototype.slice.call(productList.childNodes);
+	const prod = product.path[product.path.length - 7];
+	const dupe = items.filter(
+		(item) => item.getAttribute('id') == prod.dataset.id
+	);
+	prod.dataset.quantity = parseInt(prod.dataset.quantity) + 1;
 
-  if(dupe.length) {
-    const item = document.querySelector(`#qty-${prod.dataset.id}`);
-    document.getElementById(dupe[0].getAttribute("id")).setAttribute("data-quantity", prod.dataset.quantity);
-    item.innerHTML = `Qty: ${prod.dataset.quantity}`;
-    updateTotals();
-  } else {
-    fetch(`api/product/${prod.dataset.id}`, {
-      method: "get"
-    })
-    .then(result => {
-      return result.json();
-    })
-    .then(productData => {
-      return productData.product[0];
-    })
-    .then(data => {
-      const item = document.createElement("div");
-      const image = document.createElement("img");
-      const name = document.createElement("span");
-      const quantity = document.createElement("span");
-      const removeItem = document.createElement("img");
+	if (dupe.length) {
+		const item = document.querySelector(`#qty-${prod.dataset.id}`);
+		document
+			.getElementById(dupe[0].getAttribute('id'))
+			.setAttribute('data-quantity', prod.dataset.quantity);
+		item.innerHTML = `Qty: ${prod.dataset.quantity}`;
+		updateTotals();
+	} else {
+		fetch(`api/product/${prod.dataset.id}`, {
+			method: 'get',
+		})
+			.then((result) => {
+				return result.json();
+			})
+			.then((productData) => {
+				return productData.product[0];
+			})
+			.then((data) => {
+				const item = document.createElement('div');
+				const image = document.createElement('img');
+				const name = document.createElement('span');
+				const quantity = document.createElement('span');
+				const removeItem = document.createElement('img');
 
-      removeItem.setAttribute("id", prod.dataset.id);
-      removeItem.setAttribute("src", "/images/icons/circle-xmark-regular.svg");
-      removeItem.classList.add("remove-item");
+				removeItem.setAttribute('id', prod.dataset.id);
+				removeItem.setAttribute(
+					'src',
+					'/images/icons/circle-xmark-regular.svg'
+				);
+				removeItem.classList.add('remove-item');
 
-      removeItem.addEventListener("click", removeItem_onClick);
+				removeItem.addEventListener('click', removeItem_onClick);
 
-      item.classList.add("item");
-      item.setAttribute("id", prod.dataset.id);
+				item.classList.add('item');
+				item.setAttribute('id', prod.dataset.id);
 
-      image.setAttribute("alt", "IMG");
-      image.setAttribute("src", prod.dataset.imageurl);
-      image.classList.add("prod-img");
+				image.setAttribute('alt', 'IMG');
+				image.setAttribute('src', prod.dataset.imageurl);
+				image.classList.add('prod-img');
 
-      name.innerHTML = data.product_name;
+				name.innerHTML = data.product_name;
 
-      quantity.setAttribute("id", `qty-${prod.dataset.id}`);
-      quantity.innerHTML = `Qty: ${prod.dataset.quantity}`;
+				quantity.setAttribute('id', `qty-${prod.dataset.id}`);
+				quantity.innerHTML = `Qty: ${prod.dataset.quantity}`;
 
-      item.appendChild(image);
-      item.appendChild(name);
-      item.appendChild(quantity);
-      item.appendChild(removeItem); 
+				item.appendChild(image);
+				item.appendChild(name);
+				item.appendChild(quantity);
+				item.appendChild(removeItem);
 
-      productList.appendChild(item);
-      updateTotals();
-    })
-    .catch(err => {
-      console.log(err);
-      updateTotals();
-    });
-  }
-}
+				productList.appendChild(item);
+				updateTotals();
+			})
+			.catch((err) => {
+				console.log(err);
+				updateTotals();
+			});
+	}
+};
 
 // Create our number formatter.
 const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2,
+	style: 'currency',
+	currency: 'USD',
+	minimumFractionDigits: 2,
 
-  // These options are needed to round to whole numbers if that's what you want.
-  //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-  //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+	// These options are needed to round to whole numbers if that's what you want.
+	//minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+	//maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
 
 const updateTotals = () => {
-  const subTotal =document.querySelector(".sub-total-amount");
-  const discount = document.querySelector(".discount-amount");
-  const tax = document.querySelector(".tax-amount");
-  const grandTotal = document.querySelector(".grand-total-amount");
+	const subTotal = document.querySelector('.sub-total-amount');
+	const tax = document.querySelector('.tax-amount');
+	const grandTotal = document.querySelector('.grand-total-amount');
 
-  const items = document.querySelectorAll(".item");
-  var subtotal = parseFloat("0.00");
+	const items = document.querySelectorAll('.item');
+	var subtotal = parseFloat('0.00');
 
-  items.forEach(item => {
-    let product = document.querySelector(`.product[data-id='${item.id}']`);
-    let price = parseFloat(product.getAttribute("data-saleprice"));
-    subtotal += (price * product.getAttribute("data-quantity"));
-  });
+	items.forEach((item) => {
+		let product = document.querySelector(`.product[data-id='${item.id}']`);
+		let price = parseFloat(product.getAttribute('data-saleprice'));
+		subtotal += price * product.getAttribute('data-quantity');
+	});
 
-  var taxAmount = parseFloat("0.00");
+	var taxAmount = parseFloat('0.00');
 
-  taxAmount = subtotal * 0.13;
+	taxAmount = subtotal * 0.13;
 
-  var grandTotalAmount = parseFloat("0.00");
-  grandTotalAmount = (subtotal + taxAmount) - discountAmount;
-  if (grandTotalAmount < 0) {
-    grandTotalAmount = 0;
-  }
-  
-  subTotal.innerHTML = formatter.format(subtotal);
-  tax.innerHTML = formatter.format(taxAmount);
-  grandTotal.innerHTML = formatter.format(grandTotalAmount);
-  
-  totals.subtotal = parseFloat(subtotal.toFixed(2));
-  totals.discount = parseFloat(discountAmount.toFixed(2));
-  totals.tax = parseFloat(taxAmount.toFixed(2));
-  totals.grandtotal = parseFloat(grandTotalAmount.toFixed(2));
+	var grandTotalAmount = parseFloat('0.00');
+	grandTotalAmount = subtotal + taxAmount - discountAmount;
+	if (grandTotalAmount < 0) {
+		grandTotalAmount = 0;
+	}
 
-  updateCart();
-}
+	subTotal.innerHTML = formatter.format(subtotal);
+	tax.innerHTML = formatter.format(taxAmount);
+	grandTotal.innerHTML = formatter.format(grandTotalAmount);
+
+	totals.subtotal = parseFloat(subtotal.toFixed(2));
+	totals.tax = parseFloat(taxAmount.toFixed(2));
+	totals.grandtotal = parseFloat(grandTotalAmount.toFixed(2));
+	updateCart();
+};
 
 const applyDiscount = () => {
-  discountAmount = parseFloat(document.querySelector(".discount-amount").value);
-  let grandTotal = document.querySelector(".grand-total-amount");
-  let totalAmount = parseFloat(grandTotal.innerHTML.slice(1, grandTotal.innerHTML.length-1));
+	discountAmount = parseFloat(document.querySelector('.discount-amount').value);
+	let grandTotal = document.querySelector('.grand-total-amount');
+	let totalAmount = parseFloat(
+		grandTotal.innerHTML.slice(1, grandTotal.innerHTML.length - 1)
+	);
 
-  if (discountAmount != prev) {
-    let amount = totalAmount - discountAmount
+	if (discountAmount != prev) {
+		let amount = totalAmount - discountAmount;
+		totals.discount = discountAmount;
 
-    if (amount < 0) {
-      amount = 0;
-    }
-    prev = discountAmount;
-    grandTotal.innerHTML = formatter.format(amount);
-  }
-}
+		if (amount < 0) {
+			amount = 0;
+		}
+		prev = discountAmount;
+		grandTotal.innerHTML = formatter.format(amount);
+		updateCart();
+	}
+};
 
-products.forEach(product => {
-  product.addEventListener("click", product_onClick);
-}); 
+products.forEach((product) => {
+	product.addEventListener('click', product_onClick);
+});
 
 updateTotals();
